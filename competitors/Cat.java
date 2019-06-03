c3e7163721b0baa39d2ea5a94743e4db27b3c205
Electronic89 <e_kharionovskiy@rambler.ru>
2019-06-03 23:41:58 +0300

    1

--- competitors/Cat.java
+++
@@ -1,9 +0,0 @@
-package Lesson_1.Marafon.competitors;
-
-import Lesson_1.Marafon.competitors.Animal;
-
-public class Cat extends Animal {
-    public Cat(String name) {
-        super("Кот", name, 200, 20, 0);
-    }
-}
